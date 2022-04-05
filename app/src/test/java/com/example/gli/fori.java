b6c81286387e0fd135371c3a340ce749bc3b2322
gsy <gys6167@hotmail.com>
2022-04-05 16:00:50 +0800

    init

---
+++ app/src/test/java/com/example/gli/fori.java
@@ -0,0 +1,13 @@
+package com.example.gli;
+
+import org.junit.Test;
+
+public class fori {
+
+    @Test
+    public void test(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("gsy "+i);
+        }
+    }
+}
